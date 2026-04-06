224fd15cd1aa0ba4e25f05d73b80026f84e567b2
Vidhya-Lakshmi-T <tvidhyalakshmi1010@gmail.com>
2026-04-07 02:36:38 +0530

    Update API base URL to new address

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import requests
 import streamlit as st
 
-API_BASE = "http://localhost:8000"
+API_BASE = "http://10.80.93.95:8000"
 ROLLBACK_THRESHOLD = 70
 REFRESH_INTERVAL = 20
 DISPLAY_TZ = ZoneInfo("Asia/Kolkata")
